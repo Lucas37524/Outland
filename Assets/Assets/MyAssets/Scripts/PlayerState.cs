a9feb02309d937393e21350a30176432113a5049
Lucas37524 <lward25@student.dimanregional.org>
2025-03-21 14:04:34 -0400

    Week 7

--- Assets/Assets/MyAssets/Scripts/PlayerState.cs
+++ Assets/Assets/MyAssets/Scripts/PlayerState.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-   
     public static PlayerState Instance { get; set; }
 
     // ---- Player Health ---- //
     public float currentHealth;
     public float maxHealth;
-
 
     // ---- Player Calories ---- //
     public float currentCalories;
@@ -21,11 +19,9 @@ public class PlayerState : MonoBehaviour
 
     public GameObject playerBody;
 
-
     // ---- Player Hydration ---- //
     public float currentHydrationPercent;
     public float maxHydrationPercent;
-
 
     private void Awake()
     {
@@ -45,44 +41,64 @@ public class PlayerState : MonoBehaviour
         currentCalories = maxCalories;
         currentHydrationPercent = maxHydrationPercent;
 
-        StartCoroutine(decreaseHydration());
+        StartCoroutine(DecreaseHydration());
+        StartCoroutine(CheckHealthDecay()); // Start health decay coroutine
     }
 
-    IEnumerator decreaseHydration()
+    IEnumerator DecreaseHydration()
     {
         while (true)
         {
-            currentHydrationPercent -= 1;
+            if (currentHydrationPercent > 0)
+            {
+                currentHydrationPercent -= 1;
+            }
+
             yield return new WaitForSeconds(10);
         }
     }
 
+    IEnumerator CheckHealthDecay()
+    {
+        while (true)
+        {
+            if (currentHydrationPercent < 20 || currentCalories < 300)
+            {
+                currentHealth -= 2;
+                currentHealth = Mathf.Max(currentHealth, 0); // Ensure health doesn't go below 0
+            }
 
-    // Update is called once per frame
+            yield return new WaitForSeconds(2); // Health decreases every 5 seconds if conditions are met
+        }
+    }
+
     void Update()
     {
         distanceTravelled += Vector3.Distance(playerBody.transform.position, lastPosition);
         lastPosition = playerBody.transform.position;
 
-        if (distanceTravelled >=5)
+        if (distanceTravelled >= 4)
         {
             distanceTravelled = 0;
-            currentCalories -= 1;
+            if (currentCalories > 0)
+            {
+                currentCalories -= 1;
+            }
         }
     }
 
     public void setHealth(float newHealth)
     {
-        currentHealth = newHealth;
+        currentHealth = Mathf.Clamp(newHealth, 0, maxHealth);
     }
+
     public void setCalories(float newCalories)
     {
-        currentCalories = newCalories;
+        currentCalories = Mathf.Clamp(newCalories, 0, maxCalories);
     }
 
     public void setHydration(float newHydration)
     {
-        currentHydrationPercent = newHydration;
+        currentHydrationPercent = Mathf.Clamp(newHydration, 0, maxHydrationPercent);
     }
-
 }
